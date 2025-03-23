2919cd1e737f90fd1a625a13c73703f128e9b248
Carmel Hecht <carmel.h@taboola.com>
2025-03-23 11:23:28 +0200

    fix: images on github pages

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Base path configuration
 const BASE_PATH = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
     ? '' // Empty for local development
-    : '/codeless-site'; // For GitHub Pages
+    : '/codeless-demo'; // For GitHub Pages
 
 export { BASE_PATH }; 
